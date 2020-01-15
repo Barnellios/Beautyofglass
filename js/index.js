a378f559a586c5b0b26b36041e377b36c5726fbf
Barnellioss <lenbylorobiti@gmail.com>
2020-01-15 13:21:52 +0000

    Still

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function setNewImage(){
+document.getElementById("img1").src 
+= "img/jana_dark_2.jpg";
+}
+
+function setOldImage(){
+document.getElementById("img1").src 
+= "img/jana_dark.jpg";
+}
