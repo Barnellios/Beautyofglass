e43989ce79bce7a18662c7dcefe8219380011d1b
Barnellioss <lenbylorobiti@gmail.com>
2020-01-19 21:02:50 +0000

    Still

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 function setNewImage(){
 document.getElementById("jana").src 
 = "img/jana_dark_2.jpg";
@@ -15,4 +16,49 @@ document.getElementById("julia").src
 function setOldJulia(){
 document.getElementById("julia").src 
 = "img/julia_dark.jpg";
+}
+
+function setNewMonika(){
+document.getElementById("monik").src 
+= "img/monika_dark_2.jpg";
+}
+
+function setOldMonika(){
+document.getElementById("monik").src 
+= "img/monika_dark.jpg";
+}
+
+function setNewRose(){
+document.getElementById("peachy").src 
+= "img/peachy_rose_dark_2.jpg";
+}
+
+function setOldRose(){
+document.getElementById("peachy").src 
+= "img/peachy_rose_dark.jpg";
+}
+
+function setNewSea(){
+document.getElementById("seashell").src 
+= "img/seashell_dark_2.jpg";
+}
+
+function setOldSea(){
+document.getElementById("seashell").src 
+= "img/seashell_dark.jpg";
+}
+
+function setNewEdit(){
+document.getElementById("edit").src 
+= "img/edit_dark_2.jpg";
+}
+
+function setOldEdit(){
+document.getElementById("edit").src 
+= "img/edit.jpg";
+}
+
+function bug(){
+document.getElementById("ficha") 
+alert("Не баг а фіча :)");
 }
